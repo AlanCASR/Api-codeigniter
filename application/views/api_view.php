979d4d6d3760f046919026ef0f1716422dbc748e
Alan Castillo <alancr.edu@gmail.com>
2023-06-24 21:55:35 -0600

    Proyecto agregado

---
+++ application/views/api_view.php
@@ -0,0 +1,174 @@
+<html>
+<head>
+    <title>CRUD REST API en Codeigniter</title>
+    
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <div class="container">
+        <br />
+        <br>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-md-10">
+                        <h3 align="center">CRUD REST API en Codeigniter</h3>
+                    </div>
+                    <div class="col-md-2" >
+                        <button type="button" id="add_button" class="btn btn-info btn-xs">Registrar Usuario</button>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <span id="success_message"></span>
+                <table class="table table-bordered table-striped text-center">
+                    <thead>
+                        <tr>
+                            <th>Primer Nombre</th>
+                            <th>Apellido</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<div id="userModal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="user_form">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">X</button>
+                    <h4 class="modal-title">Añadir usuario</h4>
+                </div>
+                <div class="modal-body">
+                    <label>Nombre</label>
+                    <input type="text" name="first_name" id="first_name" class="form-control" />
+                    <span id="first_name_error" class="text-danger"></span>
+                    <br />
+                    <label>Apellido</label>
+                    <input type="text" name="last_name" id="last_name" class="form-control" />
+                    <span id="last_name_error" class="text-danger"></span>
+                    <br />
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="user_id" id="user_id" />
+                    <input type="hidden" name="data_action" id="data_action" value="Insert" />
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Añadir" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+    
+    function fetch_data()
+    {
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{data_action:'fetch_all'},
+            success:function(data)
+            {
+                $('tbody').html(data);
+            }
+        });
+    }
+
+    fetch_data();
+
+    $('#add_button').click(function(){
+        $('#user_form')[0].reset();
+        $('.modal-title').text("Registro de Usuarios");
+        $('#action').val('Registrar Usuario');
+        $('#data_action').val("Insert");
+        $('#userModal').modal('show');
+    });
+
+    $(document).on('submit', '#user_form', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:"<?php echo base_url() . 'test_api/action' ?>",
+            method:"POST",
+            data:$(this).serialize(),
+            dataType:"json",
+            success:function(data)
+            {
+                if(data.success)
+                {
+                    $('#user_form')[0].reset();
+                    $('#userModal').modal('hide');
+                    fetch_data();
+                    if($('#data_action').val() == "Insert")
+                    {
+                        $('#success_message').html
+                        (
+                            '<div class="alert alert-success alert-dismissable">Usuario Registrado<button type="button" class="close" onclick="closeAlert()">&times;</button></div>'
+                        );
+                    }
+                }
+
+                if(data.error)
+                {
+                    $('#first_name_error').html(data.first_name_error);
+                    $('#last_name_error').html(data.last_name_error);
+                }
+            }
+        })
+    });
+
+    $(document).on('click', '.edit', function(){
+        var user_id = $(this).attr('id');
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{user_id:user_id, data_action:'fetch_single'},
+            dataType:"json",
+            success:function(data)
+            {
+                $('#userModal').modal('show');
+                $('#first_name').val(data.first_name);
+                $('#last_name').val(data.last_name);
+                $('.modal-title').text('Editar Usuario');
+                $('#user_id').val(user_id);
+                $('#action').val('Editar');
+                $('#data_action').val('Edit');
+            }
+        })
+    });
+
+    $(document).on('click', '.delete', function(){
+        var user_id = $(this).attr('id');
+        if(confirm("¿Estas seguro que quieres eliminar al usuario?"))
+        {
+            $.ajax({
+                url:"<?php echo base_url(); ?>test_api/action",
+                method:"POST",
+                data:{user_id:user_id, data_action:'Delete'},
+                dataType:"JSON",
+                success:function(data)
+                {
+                    if(data.success)
+                    {
+                        $('#success_message').html('<div class="alert alert-success">Usuario Eliminado<button type="button" class="close" onclick="closeAlert()">&times;</button></div>');
+                        fetch_data();
+                    }
+                }
+            })
+        }
+    });  
+});
+</script>
